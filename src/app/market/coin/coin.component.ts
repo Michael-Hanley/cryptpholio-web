e13e32b1905f92ee94134854d1ac9c496dc5f0d9
Michael Hanley <pasaquali@gmail.com>
2018-04-18 13:41:14 -0700

    calculate marketcap in btc

--- src/app/market/coin/coin.component.ts
+++ src/app/market/coin/coin.component.ts
@@ -10,6 +10,8 @@ import {DomSanitizer} from '@angular/platform-browser';
 })
 export class CoinComponent implements OnInit {
   coin;
+  current_btc_price;
+  market_cap_btc;
   imageUrl = 'https://www.cryptocompare.com';
   constructor(private route: ActivatedRoute, iconRegistry: MatIconRegistry,
     sanitizer: DomSanitizer) {
@@ -17,6 +19,10 @@ export class CoinComponent implements OnInit {
     params => {
          console.log(params);
          this.coin = params;
+         this.current_btc_price = this.coin.usd_price / this.coin.btc_price;
+         console.log(this.current_btc_price);
+         this.market_cap_btc = this.coin.market_cap_usd / this.current_btc_price;
+         console.log(this.market_cap_btc);
     });
     iconRegistry.addSvgIcon(
       'btc',
