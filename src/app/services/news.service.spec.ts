befe635bb6f1a8b1c94a8e7b2b3f9b83bc01772b
Michael-Hanley <pasaquali@gmail.com>
2020-01-23 20:31:41 -0800

    Add news service tests

--- src/app/services/news.service.spec.ts
+++ src/app/services/news.service.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { NewsService } from './news.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('NewsService', () => {
   beforeEach(() => {
@@ -10,8 +10,22 @@ describe('NewsService', () => {
       imports: [HttpClientTestingModule]
     });
   });
-  it('should be created', () => {
-    const service: NewsService = TestBed.get(NewsService);
+  it('should be created', inject([NewsService], (service: NewsService) => {
     expect(service).toBeTruthy();
-  });
+  }));
+  it('Should return news', inject([NewsService, HttpTestingController], (service: NewsService, httpMock: HttpTestingController) => {
+    const mockNews = {
+      article: 'test-article'
+    };
+
+    service.getCoinNews('test-coin')
+      .subscribe(testNews => {
+        expect(testNews).toBe({'article': 'test-article'});
+      });
+
+    const req = httpMock.expectOne('https://min-api.cryptocompare.com/data/v2/news/?categories=test-coin');
+
+    req.flush(mockNews);
+    httpMock.verify();
+  }));
 });
